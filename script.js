33ac6098332aee831862019540c4e17a8fc5e4bc
Cvetko0o <55314876+Cvetko0o@users.noreply.github.com>
2022-04-19 19:52:10 +0300

    working code

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const form = document.getElementById('form');
 const input = document.getElementById('input');
+const todos = document.getElementsByClassName('todo-extension');
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const todo = input.value;
+    
+    if (todo) {
+
+        const todoEl = document.createElement('li');
+        todoEl.innerText = todo;
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed');
+        });
+        todos[0].appendChild(todoEl);
+        input.value = '';
+
+    }
+
+});
